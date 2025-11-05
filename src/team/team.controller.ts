d30dd177d07d3bd64f3123359ffa5ece5137b211
Sander Coemans <sander.coemans@student.ucll.be>
2025-11-05 13:14:38 +0100

    all team CRUD operations with correct error messages

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,10 +1,12 @@
 import {
     Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
   Post,
+  Put,
 } from '@nestjs/common';
 import { TeamService } from '../team/team.service.js';
 import { Team, Prisma } from '../../generated/prisma/client.js';
@@ -26,5 +28,26 @@ export class TeamController {
   @Post('/api/v1/teams')
   async createTeam(@Body() teamData: Prisma.TeamCreateInput): Promise<Team> {
     return await this.teamService.createTeam(teamData);
+  }
+
+  @Delete('/api/v1/teams/:id')
+  async deleteTeam(@Param('id', ParseIntPipe) id: number): Promise<Team> {
+    return await this.teamService.deleteTeam(id);
+  }
+
+  @Put('/api/v1/teams/add')
+  async addPokemonToTeam(
+    @Body('teamId', ParseIntPipe) teamId: number,
+    @Body('pokemonId', ParseIntPipe) pokemonId: number,
+  ): Promise<Team> {
+    return await this.teamService.addPokemonToTeam(teamId, pokemonId);
+  }
+
+  @Put('/api/v1/teams/remove')
+  async removePokemonFromTeam(
+    @Body('teamId', ParseIntPipe) teamId: number,
+    @Body('pokemonId', ParseIntPipe) pokemonId: number,
+  ): Promise<Team> {
+    return await this.teamService.removePokemonFromTeam(teamId, pokemonId);
   }
 }
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Team, Prisma } from 'generated/prisma/client.js';
 import { PrismaService } from '../prisma.service.js';
 
@@ -7,12 +7,15 @@ export class TeamService {
   constructor(private prisma: PrismaService) {}
 
   async getAllTeams(): Promise<Team[]> {
-    return await this.prisma.team.findMany();
+    return await this.prisma.team.findMany(
+      { include: { pokemons: true } }
+    );
   }
 
   async getTeamById(id: number): Promise<Team | null> {
     return await this.prisma.team.findUnique({
-      where: { id: id }
+      where: { id: id },
+      include: { pokemons: true },
     });
   }
 
@@ -27,10 +30,60 @@ export class TeamService {
     }
 
     if (pokemonCount > 6) {
-      throw new Error('A team cannot have more than 6 Pokémons.');
+      throw new BadRequestException('A team cannot have more than 6 Pokémons.');
     }
     return await this.prisma.team.create({
       data: teamData,
+      include: { pokemons: true },
+    });
+  }
+
+  async deleteTeam(id: number): Promise<Team> {
+    return await this.prisma.team.delete({
+      where: { id: id },
+    });
+  }
+
+  async addPokemonToTeam(teamId: number, pokemonId: number): Promise<Team> {
+    const team = await this.prisma.team.findUnique({
+      where: { id: teamId },
+      include: { pokemons: true },
+    });
+
+    if (team && team.pokemons.length >= 6) {
+      throw new BadRequestException('A team cannot have more than 6 Pokémons.');
+    } else if (team && team.pokemons.some(p => p.id === pokemonId)) {
+      throw new BadRequestException('This Pokémon is already in the team.');
+    }
+
+    return await this.prisma.team.update({
+      where: { id: teamId },
+      data: {
+        pokemons: {
+          connect: { id: pokemonId },
+        },
+      },
+      include: { pokemons: true },
+    });
+  }
+
+  async removePokemonFromTeam(teamId: number, pokemonId: number): Promise<Team> {
+    const team = await this.prisma.team.findUnique({
+      where: { id: teamId },
+      include: { pokemons: true },
+    });
+
+    if (team && !team.pokemons.some(p => p.id === pokemonId)) {
+      throw new BadRequestException('This Pokémon is not in the team.');
+    }
+
+    return await this.prisma.team.update({
+      where: { id: teamId },
+      data: {
+        pokemons: {
+          disconnect: { id: pokemonId },
+        },
+      },
       include: { pokemons: true },
     });
   }
